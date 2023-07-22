cc667c8d9cec9cc68a76c282c61f890958aa0a47
Jude Lanning <judelanning15@gmail.com>
2023-07-21 21:17:38 -0500

    Added color class for readability

--- repute.py
+++ repute.py
@@ -23,7 +23,7 @@ load_dotenv("api-keys.env")
 #Checks VirusTotal for vendor dispositions, communicating files, and domain resolutions associated with given IP.
 def virus_total_ip_check(ip):
     virus_total_api_key = os.getenv("virus_total_api_key")
-    print("\033[1m" + "VirusTotal Report" + "\033[0m")
+    print(colors.BOLD + "VirusTotal Report" + colors.BLACK)
 
     if virus_total_api_key == None:
         print("No Virus Total API Key Configured - Skipping Check")
@@ -43,16 +43,16 @@ def virus_total_ip_check(ip):
         harmless = parse['data']['attributes']['last_analysis_stats']['harmless']
         undetected = parse['data']['attributes']['last_analysis_stats']['undetected']
 
-        print('\033[94m' + "Vendor Dispositions:" + '\033[0m')
-        print("Malicious: " + '\033[91m' + str(malicious) + '\033[0m')
-        print("Suspicious: " + '\033[93m' + str(suspicious) + '\033[0m')
-        print("Harmless/Undetected: " + '\033[92m' + str(harmless + undetected) + '\033[0m')
+        print(colors.BLUE + "Vendor Dispositions:" + colors.BLACK)
+        print("Malicious: " + colors.RED + str(malicious) + colors.BLACK)
+        print("Suspicious: " + colors.YELLOW + str(suspicious) + colors.BLACK)
+        print("Harmless/Undetected: " + colors.GREEN + str(harmless + undetected) + colors.BLACK)
 
         url = "https://www.virustotal.com/api/v3/ip_addresses/" + ip + "/communicating_files?limit=50"
         response = requests.get(url, headers=headers)
         parse = json.loads(response.text)
 
-        print('\033[94m' + '\nCommunicating Files' + '\033[0m' + " (" + str(parse["meta"]["count"]) + "):")
+        print(colors.BLUE + '\nCommunicating Files' + colors.BLACK + " (" + str(parse["meta"]["count"]) + "):")
         for f in parse["data"]:
             malicious = f["attributes"]["last_analysis_stats"]["malicious"]
             suspicious = f["attributes"]["last_analysis_stats"]["suspicious"]
@@ -60,15 +60,15 @@ def virus_total_ip_check(ip):
             undetected = f["attributes"]["last_analysis_stats"]["undetected"]
 
             if malicious or suspicious > 0:
-                print(f["attributes"]["md5"] + " (" + '\033[91m' + (str(malicious + suspicious)) + "\033[0m" + "/" + str(malicious + suspicious + harmless + undetected) +")")
+                print(f["attributes"]["md5"] + " (" + colors.RED + (str(malicious + suspicious)) + colors.BLACK + "/" + str(malicious + suspicious + harmless + undetected) +")")
             else:
-                print(f["attributes"]["md5"] + " (" + '\033[92m' + (str(malicious + suspicious)) + "\033[0m" + "/" + str(malicious + suspicious + harmless + undetected) +")")
+                print(f["attributes"]["md5"] + " (" + colors.GREEN + (str(malicious + suspicious)) + colors.BLACK + "/" + str(malicious + suspicious + harmless + undetected) +")")
 
         url = "https://www.virustotal.com/api/v3/ip_addresses/" + ip  + "/resolutions?limit=40"
         response = requests.get(url, headers=headers)
         parse = json.loads(response.text)
 
-        print('\033[94m' + "\nDomain Resolutions" + '\033[0m' + " (" + str(parse["meta"]["count"]) + "):")
+        print(colors.BLUE + "\nDomain Resolutions" + colors.BLACK + " (" + str(parse["meta"]["count"]) + "):")
         for f in parse['data']:
             
             malicious = f["attributes"]["host_name_last_analysis_stats"]["malicious"]
@@ -77,14 +77,14 @@ def virus_total_ip_check(ip):
             undetected = f["attributes"]["host_name_last_analysis_stats"]["undetected"]
             
             if malicious or suspicious > 0:
-                print(f["id"] + " (" + '\033[91m' + str(malicious + suspicious) + "\033[0m" +"/" + str(malicious + suspicious + harmless + undetected) + ")")
+                print(f["id"] + " (" + colors.RED + str(malicious + suspicious) + colors.BLACK +"/" + str(malicious + suspicious + harmless + undetected) + ")")
             else:
-                print(f["id"] + " (" + '\033[92m' + str(malicious + suspicious) + "\033[0m" +"/" + str(malicious + suspicious + harmless + undetected) + ")")
+                print(f["id"] + " (" + colors.GREEN + str(malicious + suspicious) + colors.BLACK +"/" + str(malicious + suspicious + harmless + undetected) + ")")
 
 #Checks GreyNoise for IP classification, noise, RIOT status, and last seen date.
 def greynoise_ip_check(ip):
     greynoise_api_key = os.getenv("greynoise_api_key")
-    print("\033[1m" + "\nGreyNoise Report" + "\033[0m")
+    print(colors.BOLD + "\nGreyNoise Report" + colors.BLACK)
 
     if greynoise_api_key == None:
         print("No Greynoise API Key Configured - Skipping Check")
@@ -101,9 +101,9 @@ def greynoise_ip_check(ip):
 
         try:
             classification = parse["classification"]
-            print('\033[94m' + "Classification: " + '\033[0m' + classification)
+            print(colors.BLUE + "Classification: " + colors.BLACK + classification)
         except:
-            print('\033[94m' + "Classification:" + '\033[0m' + " None")
+            print(colors.BLUE + "Classification:" + colors.BLACK + " None")
 
         noise = parse["noise"]
         riot = parse["riot"]
@@ -118,18 +118,18 @@ def greynoise_ip_check(ip):
         except:
             last_seen = "Unknown"
 
-        print('\033[94m' + "Noise: " + '\033[0m' + str(noise))
+        print(colors.BLUE + "Noise: " + colors.BLACK + str(noise))
         
         if riot == True:
-            print('\033[94m' + "RIOT: " + '\033[0m' + str(riot) + ", " + name)
+            print(colors.BLUE + "RIOT: " + colors.BLACK + str(riot) + ", " + name)
         else:
-            print('\033[94m' + "RIOT:" + '\033[0m' + " False")
+            print(colors.BLUE + "RIOT:" + colors.BLACK + " False")
         
-        print('\033[94m' + "Last Seen: " + '\033[0m' + str(last_seen))
+        print(colors.BLUE + "Last Seen: " + colors.BLACK + str(last_seen))
 
 #Gets list of Tor exit node IPs from TorProject and checks if provided IP is in the list.
 def tor_exit_node_check(ip):
-    print("\033[1m" + "\nTor Check" + "\033[0m")
+    print(colors.BOLD + "\nTor Check" + colors.BLACK)
     try:
         with urllib.request.urlopen('https://check.torproject.org/torbulkexitlist') as f:
             ips_string = f.read().decode('utf-8')
@@ -137,9 +137,9 @@ def tor_exit_node_check(ip):
             ips_list = list(ips_string.split("\n"))
 
             if ip in ips_list:
-                print('\033[94m' + "Tor Exit Node:" + '\033[0m' + " True")
+                print(colors.BLUE + "Tor Exit Node:" + colors.BLACK + " True")
             else:
-                print('\033[94m' + "Tor Exit Node:" + '\033[0m' + " False")
+                print(colors.BLUE + "Tor Exit Node:" + colors.BLACK + " False")
     except:
         print("Couldn't Reach Tor Exit Node List")
 
@@ -170,6 +170,15 @@ def check_ip(ip):
     if ipaddress.ip_address(ip).is_private == True:
         print(ip + " is a private IP address. Please try again with a public IP address.")
         exit()
+
+#Used to change terminal text color.
+class colors:
+    BLACK = "\033[0m"
+    BOLD = "\033[1m"
+    BLUE = "\033[94m"
+    RED = "\033[91m"
+    YELLOW = "\033[93m"
+    GREEN = "\033[92m"
 
 #Starts script.
 start_script()
